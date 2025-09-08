2b438b2079265c64f91ce6615658a94e62a24daa
Phalla <lalaphalla@gmail.com>
2025-09-08 10:06:44 +0700

    add: create CRUD for customer with backpack

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,9 +13,19 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Customer extends Model
 {
+    use CrudTrait;
     /** @use HasFactory<\Database\Factories\CustomerFactory> */
     use HasFactory;
 
+    protected $fillable = [
+        'name',
+        'email',
+        'type',
+        'address',
+        'city',
+        'state',
+        'postal_code',
+    ];
     /**
      * 
      *
